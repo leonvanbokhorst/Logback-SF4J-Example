6af792694839f5318be540433d7c9ebd0c88d2fb
Leon van Bokhorst <leonvanbokhorst@users.noreply.github.com>
2018-11-10 13:11:28 +0100

    Init

---
+++ src/main/java/com/leonvanbokhorst/logdemo/LogDemo.java
@@ -0,0 +1,15 @@
+package com.leonvanbokhorst.logdemo;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+class LogDemo {
+
+  private final Logger logger = LoggerFactory.getLogger(this.getClass());
+  private int messages = 0;
+
+  void logSomething(String message) {
+    logger.debug("Message {}: {}", messages, message);
+    messages++;
+  }
+}
---
+++ src/test/java/com/leonvanbokhorst/logdemo/LogDemoTest.java
@@ -0,0 +1,37 @@
+package com.leonvanbokhorst.logdemo;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class LogDemoTest {
+
+  private LogDemo sut;
+
+  @Before
+  public void setUp() {
+    // assign
+    this.sut = new LogDemo();
+  }
+
+  @Test
+  public void shouldLogSomething() {
+    // act
+    this.sut.logSomething("Log baby, yeah!");
+
+    // assert
+    assertTrue(true);
+  }
+
+  @Test
+  public void shouldLogMoreThings() {
+    // act
+    sut.logSomething("We've got a logger.");
+    sut.logSomething("We've got another logger.");
+    sut.logSomething("We've got a couple of loggers.");
+
+    // assert
+    assertTrue(true);
+  }
+}
